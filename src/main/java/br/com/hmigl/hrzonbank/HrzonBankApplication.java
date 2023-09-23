68e9391e54c1d9a3faaab974721a758069d9c887
hmigl <88746406+hmigl@users.noreply.github.com>
2023-09-23 11:17:57 -0300

    Initial commit

---
+++ src/main/java/br/com/hmigl/hrzonbank/HrzonBankApplication.java
@@ -0,0 +1,12 @@
+package br.com.hmigl.hrzonbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrzonBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrzonBankApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/hmigl/hrzonbank/HrzonBankApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.hmigl.hrzonbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrzonBankApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
